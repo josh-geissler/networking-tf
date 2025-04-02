e6d66d98051e2a781a85fbe9c3f5cd9722720866
Joshua Geissler <jgeissler14@gmail.com>
2025-04-02 09:30:26 -0700

    change cidr to 10.0.2.0

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "main_vpc" {
 # Create a public subnet
 resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.main_vpc.id
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "10.0.2.0/24"
     map_public_ip_on_launch = true
     availability_zone = "${var.region}a"
     tags = {
